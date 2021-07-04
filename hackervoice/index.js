4b221b38a4ed995fbf32048561e536f7ed801406
Marc Duiker <marcduiker@gmail.com>
2021-07-04 19:47:49 +0200

    Add hackervoice function

---
+++ hackervoice/index.js
@@ -0,0 +1,11 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const pw = req.query.password;
+    const responseMessage = pw
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}

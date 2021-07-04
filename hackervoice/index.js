36c5a17a2e86ac244d603d17ad667695c2eb2a39
Marc Duiker <marcduiker@gmail.com>
2021-07-04 20:27:53 +0200

    Update function

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,7 +2,11 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const pw = req.query.password;
-    const responseMessage = pw;
+    let responseMessage = "Access denied.";
+    if (pw == "letmein")
+    {
+        responseMessage = "Access granted."
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
